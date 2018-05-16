fe1eb72c05442ee277f8728a187badc6d9301773
anilpearl <anilpearl2103@gmail.com>
2018-05-16 16:09:35 +0530

    Initial Commit

---
+++ indexval.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * Project Name : Pearl Academy CRD Management
+ * Started On   : 18 August 2014
+ * Developed By : Kuuon Media Pvt Ltd 
+ */
+require 'libs/Bootstrap.php';
+require 'libs/Controller.php';
+require 'libs/Model.php';
+require 'libs/View.php';
+
+require 'libs/Database.php';
+require 'libs/Session.php';
+
+require 'config/paths.php';
+require 'config/database.php';
+
+$app = new Bootstrap();
+
+//max_allowed_packet==32M;
+//SET GLOBAL max_allowed_packet=32M
+
+
+?>
